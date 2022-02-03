b0eaf38c2ee0c3b9607bc6af7f289f6067922f2f
kevin wu <kevin6991517@icloud.com>
2022-02-04 01:46:34 +0800

    add fish shell config

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+#    set -g theme_color_scheme terminal2
+    set -g theme_display_user no
+    set -g theme_display_hostname no
+    set -g theme_newline_cursor yes
+    set -g theme_display_git_default_branch yes
+    set -g fish_prompt_pwd_dir_length 0
+end
